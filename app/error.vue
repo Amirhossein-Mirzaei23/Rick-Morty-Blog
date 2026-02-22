99f97b32d29b5b39c134b3fcdedd0d88e2c54304
Amirhossien Mirzaei <amir.hmirzaei.progra@gmail.com>
2026-02-22 21:01:11 +0330

    feat(error.vue) refactor code and imrpove structure

--- app/error.vue
+++ app/error.vue
@@ -1,19 +1,26 @@
 <script setup lang="ts">
 import type { NuxtError } from '#app'
-
+onUnmounted(() => {
+  clearError()
+})
 interface Props {
   error: NuxtError
 }
 
 const { error } = defineProps<Props>()
 
-const handleError = () => clearError({ redirect: '/' })
+useSeoMeta({
+  title: () => `${error.statusCode} - ${error.statusMessage}`,
+  description: 'An error occurred while processing your request.',
+})
+
+const handleError = () => clearError({ redirect: '/search' })
 </script>
 
 <template>
-  <div class="flex min-h-screen flex-col items-center justify-center gap-8 px-4 text-center">
+  <div class="flex min-h-dvh flex-col items-center justify-center gap-8 px-4 text-center">
     <div>
-      <p class="text-8xl font-extrabold text-primary-600">{{ error.statusCode }}</p>
+      <p class="text-primary-600 text-8xl font-extrabold">{{ error.statusCode }}</p>
       <h1 class="mt-4 text-3xl font-bold text-gray-900 dark:text-white">
         {{ error.statusCode === 404 ? 'Page not found' : 'Something went wrong' }}
       </h1>
