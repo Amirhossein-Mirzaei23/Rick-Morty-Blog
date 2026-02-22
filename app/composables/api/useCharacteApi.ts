864bb816dad0319b3412b8adcc878237b255c8e9
Amirhossien Mirzaei <amir.hmirzaei.progra@gmail.com>
2026-02-22 21:11:38 +0330

    feat(character detail) create character api hook

---
+++ app/composables/api/useCharacteApi.ts
@@ -0,0 +1,32 @@
+import type { Character } from '~/types/charecter.type'
+
+export async function useCharacteApi(characterId: Ref<string> | ComputedRef<string>) {
+  const { data: character, error: characterError } = await useFetch<Character>(
+    `https://rickandmortyapi.com/api/character/${characterId.value}`,
+    {
+      retry: 2,
+      retryDelay: 500,
+      key: `character-${characterId.value}`,
+    },
+  )
+
+  if (characterError.value) {
+    let status = characterError.value?.status ?? characterError.value?.statusCode ?? 500
+    if (characterError.value.message.includes('no response')) {
+      status = 429
+    }
+    throw createError({
+      statusCode: status || 500,
+      statusMessage:
+        status === 404
+          ? 'Character Not Found'
+          : characterError.value.message || 'Failed to Load Character',
+      fatal: true,
+    })
+  }
+
+  return {
+    character,
+    characterError,
+  }
+}
