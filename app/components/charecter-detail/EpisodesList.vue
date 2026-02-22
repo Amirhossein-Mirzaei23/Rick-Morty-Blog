3d905eb8671695d5591027a907ab9b743b634f3e
Amirhossien Mirzaei <amir.hmirzaei.progra@gmail.com>
2026-02-22 21:27:40 +0330

    feat(character detail) create episdoes list and location info and character detail page  header

---
+++ app/components/charecter-detail/EpisodesList.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import episodesIcon from '@/assets/icons/featured_play_list.svg'
+import type { Episode } from '~/types/charecter.type'
+import SectionHeader from './ui/SectionHeader.vue'
+import EpisodeCard from './ui/CharacterInfoCard.vue'
+interface Props {
+  episodes: Episode[]
+  loading?: boolean
+}
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <div class="flex flex-col gap-5">
+    <SectionHeader :image="episodesIcon" title="Episodes" alt="episodes" />
+
+    <ul class="grid grid-cols-1 gap-4 lg:grid-cols-4 lg:gap-6">
+      <template v-if="!loading && props.episodes && props.episodes.length > 0">
+        <li v-for="episode in props.episodes" :key="episode.id">
+          <EpisodeCard :title="episode.name" :subtitle="episode.episode" :label="episode.air_date" />
+        </li>
+      </template>
+      <template v-else-if="!loading && (!props.episodes || props.episodes.length === 0)">
+        <div class="text-gray-500">
+          <p>No episodes available.</p>
+        </div>
+      </template>
+      <template v-else>
+        <li v-for="n in 4" :key="n">
+          <CardSkeletonLoader :show-image-loader="false" card-class="bg-[#0AD7F10A]" loader-class="bg-base" />
+        </li>
+      </template>
+    </ul>
+  </div>
+</template>
---
+++ app/components/charecter-detail/LocationInfo.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import locationsIcon from '@/assets/icons/location_on.svg'
+import type { LocationDetail } from '~/types/charecter.type'
+import LocationCard from './ui/CharacterInfoCard.vue'
+import SectionHeader from './ui/SectionHeader.vue'
+
+interface Props {
+  location: LocationDetail | null
+  loading?: boolean
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <div class="flex flex-col gap-5">
+    <SectionHeader :image="locationsIcon" title="Locations" alt="locations" />
+    <div v-if="loading" class="grid grid-cols-1 gap-4 lg:grid-cols-4">
+      <CardSkeletonLoader :show-image-loader="false" card-class="bg-[#0AD7F10A]" loader-class="bg-base" />
+    </div>
+    <div v-else-if="!props.location">
+      <p class="text-info">No location available.</p>
+    </div>
+    <div v-else class="grid grid-cols-1 gap-4 lg:grid-cols-4">
+      <LocationCard :title="props.location.name" :subtitle="props.location.type" :label="props.location.dimension" />
+    </div>
+  </div>
+</template>
