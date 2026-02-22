ce504665d08b767d636b58f073009876a987d7ce
Amirhossien Mirzaei <amir.hmirzaei.progra@gmail.com>
2026-02-22 21:27:50 +0330

    feat(character card)

---
+++ app/components/search/CharacterCard.vue
@@ -0,0 +1,87 @@
+<script setup lang="ts">
+import type { Character, CharacterStatus } from '~/types/charecter.type'
+const { character } = defineProps<{ character: Character }>()
+const router = useRouter()
+
+function navigate() {
+  const slug = slugify(character.name)
+  router.push({
+    path: `/character/${slug}/${character.id}`,
+    query: { id: character.id },
+  })
+}
+
+function handleKeydown(e: KeyboardEvent) {
+  if (e.key === 'Enter' || e.key === ' ') {
+    e.preventDefault()
+    navigate()
+  }
+}
+
+const statusConfig: Record<CharacterStatus, { label: string; dot: string }> = {
+  Alive: {
+    label: 'Alive',
+    dot: 'bg-success',
+  },
+  Dead: {
+    label: 'Dead',
+    dot: 'bg-danger',
+  },
+  unknown: {
+    label: 'Unknown',
+
+    dot: 'bg-gray-400',
+  },
+}
+
+const status = computed(() => statusConfig[character.status] ?? statusConfig.unknown)
+
+const imageLoading = ref(false)
+const imgRef = ref<HTMLImageElement>()
+const setImageLoading = (loading: boolean) => {
+  imageLoading.value = loading
+}
+
+onMounted(() => {
+  if (imgRef.value?.complete) {
+    setImageLoading(false)
+  }
+})
+</script>
+
+<template>
+  <NuxtLink :to="`/character/${slugify(character.name)}/${character.id}`">
+    <article
+      class="group flex cursor-pointer flex-col gap-4 overflow-hidden rounded-2xl border border-[#404244] p-4"
+      tabindex="0"
+      :aria-label="`View details for ${character.name}`"
+      role="button"
+      @click="navigate"
+      @keydown="handleKeydown"
+    >
+      <div class="relative aspect-square overflow-hidden rounded-xl">
+        <div v-if="!imageLoading" class="animate-skeleton absolute inset-0" aria-hidden="true" />
+        <img
+          :src="character.image"
+          :alt="character.name"
+          loading="lazy"
+          class="h-full w-full object-cover transition duration-300 group-hover:scale-105"
+          :class="imageLoading ? 'opacity-100' : 'opacity-0'"
+          width="300"
+          height="300"
+          ref="imgRef"
+          @load="imageLoading = true"
+        />
+      </div>
+      <div class="flex flex-1 flex-col gap-3">
+        <h2 class="truncate text-base font-semibold dark:text-white">
+          {{ character.name }}
+        </h2>
+        <span class="text-md inline-flex w-fit items-center gap-2 rounded-full leading-none font-normal">
+          <span class="h-3 w-3 rounded-full" :class="status.dot" aria-hidden="true" />
+          {{ status.label }} - {{ character.species }}
+        </span>
+      </div>
+    </article>
+  </NuxtLink>
+</template>
